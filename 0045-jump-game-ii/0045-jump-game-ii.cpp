14c6cdac9c968f981e34aa7f7c9ed8da45bcf71e
Vaibhav Chauhan <104888876+Vaibhav0911@users.noreply.github.com>
2024-10-02 15:55:37 +0530

    Time: 5 ms (91.95%), Space: 19.4 MB (41.12%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -5,22 +5,24 @@ public:
         for(int i=0 ; i<n ; i++){
             if(i == n-1)              return ans;
             else if(nums[i] >= n-1)   return ++ans;
-            int mx=0,val=nums[i],l=i+1;
-            for(int j=l ; j<l+val ; j++){
-                if(j == n-1){
+            else {
+                int mx=0,val=nums[i],l=i+1;
+                for(int j=l ; j<l+val ; j++){
+                  if(j == n-1){
                     ans++;
                     return ans;
-                }  
-                if(j+nums[j] >= n-1){
+                  }  
+                  if(j+nums[j] >= n-1){
                     ans += 2;
                     return ans;
-                }
-                if(nums[j]+j > mx && nums[j] != 0){
+                  }
+                  if(nums[j]+j > mx && nums[j] != 0){
                     mx = nums[j]+j;
                     i=j-1;
+                  }
                 }
+                ans++;
             }
-            ans++;
         }
         return ans;
     }
