163d6f2aa1c6b020631d988b961fd29e7332d9c6
Vaibhav Chauhan <104888876+Vaibhav0911@users.noreply.github.com>
2024-12-12 16:15:20 +0530

    Time: 896 ms (5.10%), Space: 20.8 MB (11.97%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    
+    vector<vector<string>> ans;
+    
+    void check(vector<string> arr, int n, int i){
+        
+        if(i==n){
+            for(int j=0 ; j<n ; j++){
+                for(int k=0 ; k<n ; k++){ 
+                    if(arr[j][k] == 'F')    arr[j][k]='.';
+                }
+            }
+
+            ans.push_back(arr);
+            
+            return;
+        }
+        
+        for(int j=0 ; j<n ; j++){
+            if(arr[i][j] == 'F'){
+                continue;
+            }
+            else{
+                
+                vector<string> arr1 = arr;
+                arr[i][j] = 'Q';
+                
+                for(int k=i+1 ; k<n ; k++)      arr[k][j]='F';
+                for(int k=i+1, l=j+1 ; k<n && l<n ; k++, l++)    arr[k][l]='F';
+                for(int k=i+1, l=j-1 ; k<n && l>=0 ; k++, l--)   arr[k][l]='F';
+                
+                for(int l=0 ; l<n ; l++){
+                  for(int k=0 ; k<n ; k++){ 
+                     cout<<arr[l][k]; 
+                  }
+                  cout<<endl;  
+                }
+                cout<<endl;
+                
+                check(arr, n, i+1);
+                
+                arr = arr1;
+                arr[i][j] = 'F';
+                
+            }
+        }
+        return;
+        
+    }
+    
+    vector<vector<string>> solveNQueens(int n) {
+        
+        vector<string> s;
+        string str="";
+        for(int i=0 ; i<n ; i++)         str += '.';
+        for(int j=0 ; j<n ; j++)         s.push_back(str);        
+        
+        check(s, n, 0);
+        return ans;
+    }
+};
