1af4931ae2128bd59674f0d85258fe0bf0266ca3
Vaibhav Chauhan <104888876+Vaibhav0911@users.noreply.github.com>
2024-12-16 18:40:26 +0530

    Time: 179 ms (9.40%), Space: 175.7 MB (5.60%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    
+    vector<vector<string>> ans;
+    
+    string cp(string s, int i, int j){
+        int l=i,h=j;
+        while(l<h){
+            if(s[l] != s[h])    return "/";
+            l++; h--;
+        }
+        string str="";
+        for( ; i<=j ; i++)      str += s[i];
+        return str;
+    }
+    
+    void check(string s, int i, vector<string> v){
+        if(i == s.length()){
+            ans.push_back(v);
+            return;
+        }
+        for(int j=i ; j<s.length() ; j++){
+            string str = cp(s, i, j);
+            if(str != "/"){
+                // cout<<str<<endl;
+                v.push_back(str);
+                check(s, j+1, v);
+                v.pop_back();
+            }
+        }
+    }
+    
+    vector<vector<string>> partition(string s) {
+        
+        vector<string> v;
+        check(s, 0, v);
+        return ans;
+    }
+};
