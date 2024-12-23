d28dc82d26b768e3dd11d6c8bbeb91c6b2ef0c22
Vaibhav Chauhan <104888876+Vaibhav0911@users.noreply.github.com>
2024-12-23 14:10:34 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (24.01%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        
+        while(num/10 > 0){
+            num = num/10 + num%10;
+        }
+        return num;
+        
+    }
+};
